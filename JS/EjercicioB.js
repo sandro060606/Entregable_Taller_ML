82a1a3cc611bf39571fdad573e06f8db8ddf6f29
Sandro Rodriguez Anchante <sandro661528@gmail.com>
2025-11-05 21:46:48 -0500

    Clasificador DoodleNet implementado y funcional para el Ejercicio B, Con sus respectivos Archivos HTML, JS Y CSS

---
+++ JS/EjercicioB.js
@@ -0,0 +1,73 @@
+const listaUsuarios = [
+  { nombre: "elena", clave: "654321", codigo: "book" },
+  { nombre: "pablito", clave: "abc12345", codigo: "rain" },
+  { nombre: "pedro", clave: "pedrito01", codigo: "door" },
+];
+
+const formulario = document.querySelector("#form-login");
+const usuario = document.querySelector("#usuario");
+const password = document.querySelector("#password");
+const resultsDiv = document.querySelector("#results");
+const mlLabelDiv = document.querySelector("#ml-label");
+const clearButton = document.getElementById("clear-btn");
+
+let classifier;
+let canvas;
+let CanvasLabel = "";
+
+function preload() {
+  classifier = ml5.imageClassifier("DoodleNet");
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.parent("contenedor-canvas");
+  background(255);
+  classifier.classifyStart(canvas, gotResult);
+  strokeWeight(12);
+  stroke(0);
+  clearButton.addEventListener("click", clearCanvas);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+}
+
+function gotResult(results) {
+  let label = results[0].label;
+  let confidence = results[0].confidence;
+  CanvasLabel = label;
+  mlLabelDiv.innerHTML = `Dibujaste: <b>${label}</b> (${nf(
+    confidence * 100,
+    0,
+    2
+  )}%)`;
+}
+
+function clearCanvas() {
+  background(255);
+  CanvasLabel = "";
+  resultsDiv.innerHTML = "";
+}
+
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const codigoDibujo = CanvasLabel.trim();
+
+  for (let i = 0; i < listaUsuarios.length; i++) {
+    const user = listaUsuarios[i];
+    if (listaUsuarios[i].nombre === usuario.value) {
+      if (
+        listaUsuarios[i].clave === password.value &&
+        listaUsuarios[i].codigo === codigoDibujo
+      ) {
+        alert(`¡Bienvenido ${user.nombre}! Acceso concedido`);
+        return;
+      }
+    }
+  }
+  alert(`Login fallido. Usuario, contraseña o código incorrecto`);
+});
